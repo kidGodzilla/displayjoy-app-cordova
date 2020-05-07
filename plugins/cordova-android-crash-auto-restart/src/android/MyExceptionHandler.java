e87bf0af6fa9257f33121dd17b864c5f5b2db333
kidGodzilla <futhey@gmail.com>
2020-05-07 14:16:53 +0800

    use my own fork

--- plugins/cordova-android-crash-auto-restart/src/android/MyExceptionHandler.java
+++ plugins/cordova-android-crash-auto-restart/src/android/MyExceptionHandler.java
@@ -6,38 +6,48 @@ import android.content.Intent;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.util.Log;
 
 public class MyExceptionHandler implements
         java.lang.Thread.UncaughtExceptionHandler {
     private final Context myContext;
     private final Class<?> myActivityClass;
     private Activity myActivity;
-           
+
     public MyExceptionHandler(Activity a, Context context, Class<?> c) {
 
         myContext = context;
         myActivityClass = c;
-        myActivity = a;         
-                
+        myActivity = a;
     }
 
     public void uncaughtException(Thread thread, Throwable exception) {
+
+        Intent intent = myContext.getPackageManager().getLaunchIntentForPackage(myContext.getPackageName() );
+
+        intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(KEY_RESTART_INTENT, intent);
+        myContext.startActivity(intent);
+        
+        if (myContext instanceof Activity) {
+          ((Activity) myContext).finish();
+        }
+
+        Runtime.getRuntime().exit(0);
+        System.exit(2);
             
                   
-    Log.e("", "restarting app");
-    Intent restartIntent = myContext.getPackageManager()
-            .getLaunchIntentForPackage(myContext.getPackageName() );
-    PendingIntent intent = PendingIntent.getActivity(
-            myContext, 0,
-            restartIntent, Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    AlarmManager manager = (AlarmManager) myContext.getSystemService(myContext.ALARM_SERVICE);
-    manager.set(AlarmManager.RTC, System.currentTimeMillis() + 1, intent);
-    System.exit(2);
+        /* Log.e("", "restarting app");
+        Intent restartIntent = myContext.getPackageManager()
+                .getLaunchIntentForPackage(myContext.getPackageName() );
+        PendingIntent intent = PendingIntent.getActivity(
+                myContext, 0,
+                restartIntent, Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        AlarmManager manager = (AlarmManager) myContext.getSystemService(myContext.ALARM_SERVICE);
+        manager.set(AlarmManager.RTC, System.currentTimeMillis() + 1, intent);
+        System.exit(2); */
             
-      
             
-        /*    
+        /* Intent intent = new Intent(myActivity, myActivityClass);
         intent.putExtra("crash", true);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
                 | Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -50,9 +60,6 @@ public class MyExceptionHandler implements
 
 //        android.os.Process.killProcess(android.os.Process.myPid());
         myActivity.finish();
-        System.exit(2);
-        */
+        System.exit(2); */
     }
-        
-  
 }
