a0772f9c93f86d5ec4942d1ee70c6efa3781b341
kidGodzilla <futhey@gmail.com>
2020-05-07 13:20:40 +0800

    use fork of cordova plugin

--- plugins/cordova-android-crash-auto-restart/src/android/MyExceptionHandler.java
+++ plugins/cordova-android-crash-auto-restart/src/android/MyExceptionHandler.java
@@ -6,23 +6,38 @@ import android.content.Intent;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.util.Log;
 
 public class MyExceptionHandler implements
         java.lang.Thread.UncaughtExceptionHandler {
     private final Context myContext;
     private final Class<?> myActivityClass;
     private Activity myActivity;
-
+           
     public MyExceptionHandler(Activity a, Context context, Class<?> c) {
 
         myContext = context;
         myActivityClass = c;
-        myActivity = a;
+        myActivity = a;         
+                
     }
 
     public void uncaughtException(Thread thread, Throwable exception) {
-
-        Intent intent = new Intent(myActivity, myActivityClass);
+            
+                  
+    Log.e("", "restarting app");
+    Intent restartIntent = myContext.getPackageManager()
+            .getLaunchIntentForPackage(myContext.getPackageName() );
+    PendingIntent intent = PendingIntent.getActivity(
+            myContext, 0,
+            restartIntent, Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    AlarmManager manager = (AlarmManager) myContext.getSystemService(myContext.ALARM_SERVICE);
+    manager.set(AlarmManager.RTC, System.currentTimeMillis() + 1, intent);
+    System.exit(2);
+            
+      
+            
+        /*    
         intent.putExtra("crash", true);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
                 | Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -36,5 +51,8 @@ public class MyExceptionHandler implements
 //        android.os.Process.killProcess(android.os.Process.myPid());
         myActivity.finish();
         System.exit(2);
+        */
     }
+        
+  
 }
